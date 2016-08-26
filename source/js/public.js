4dd8eccbdeec3c0f40583f8eb451b0a1c6ec4245
chendind <3110100527@zju.edu.cn>
2016-08-26 18:56:53 +0800

    tb

--- source/js/public.js
+++ source/js/public.js
@@ -1,8 +1,13 @@
 $(function(){
-	$("a").bind('click',function(e){
+	mui("body").on('tap','a',function(e){
 		e.preventDefault();
 		var href = $(this).attr("href");
-		var title = $(this).attr("title")
-		Bridge.openMobileWindow(href, title, function (result) {});
+		var title = $(this).attr("title");
+		if(href){
+			Bridge.openMobileWindow(href, title, function (result) {});
+		}
+		else{
+			mui.toast("建设中，敬请期待");
+		}
 	});
 });
