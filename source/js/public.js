be7c07bb38f568f3fe05a377d005350382f2ffa9
RainworD <673227139@qq.com>
2016-08-31 09:30:45 +0800

    tb

--- source/js/public.js
+++ source/js/public.js
@@ -1,10 +1,13 @@
 $(function(){
+	var height=$(window).height();
+  	$(".mui-content").css("min-height",height);
 	mui("body").on('tap','a',function(e){
 		e.preventDefault();
 		var href = $(this).attr("href");
 		var title = $(this).attr("title");
 		if(href){
-			Bridge.openMobileWindow(href, title, function (result) {});
+			// Bridge.openMobileWindow(href, title, function (result) {});
+			window.location.href=href;
 		}
 		else{
 			mui.toast("建设中，敬请期待");
